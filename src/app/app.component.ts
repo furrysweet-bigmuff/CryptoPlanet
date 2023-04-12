0a378004731f1f04e40fb9939b787b1f8993f2e9
furrysweet-bigmuff <greyhotel2@gmail.com>
2023-04-12 16:06:58 +0200

    Feature(fonts): set up material custom fonts to figma design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CryptoPlanet';
+  // private isDark = true;
+
+  // @HostBinding('class')
+  // get themeMode() {
+    // return this.isDark ? 'theme-dark' : 'theme-light';
+  // }
 }
